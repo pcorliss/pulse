6ff7d67fc02d3569f32da3cee15bfd5b1ff9e31d
Philip Corliss <pcorliss@50projects.com>
2016-04-23 09:58:24 -0500

    Basic answering working.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,6 @@
 class AnswersController < ApplicationController
+  helper AnswersHelper
+
   def new
     @survey = Survey
       .preload(:questions)
@@ -6,6 +8,13 @@ class AnswersController < ApplicationController
   end
 
   def create
-    @survey = Survey.find_by_slug!(params[:survey_id])
+    @survey = Survey
+      .preload(:questions)
+      .find_by_slug!(params[:survey_id])
+    @survey.questions.each do |question|
+      question.answers.create(survey: @survey, response: params[question.id.to_s])
+    end
+
+    redirect_to survey_path @survey.slug
   end
 end
