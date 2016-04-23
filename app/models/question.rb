40362f3a411f67a49b45b20949b4993e666b4bfc
Philip Corliss <pcorliss@50projects.com>
2016-04-23 11:44:38 -0500

    Safe Stats

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,9 @@ class Question < ApplicationRecord
   has_many :answers
 
   def stats(survey)
+    return @stats if @stats
     # We filter here instead of doing a where query to leverage preloading
-    @states ||= answers.select {|a| a.survey_id == survey.id }.map(&:response).compact.descriptive_statistics
+    responses = answers.select {|a| a.survey_id == survey.id }.map(&:response).compact.extend(DescriptiveStatistics)
+    @stats = responses.descriptive_statistics
   end
 end
