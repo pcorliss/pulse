967d9e3364e22a9a5b5d628739414d12800068eb
Philip Corliss <pcorliss@50projects.com>
2016-04-22 23:13:15 -0500

    Fixing loading, and showing survey results.

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -2,6 +2,6 @@ class SurveysController < ApplicationController
   def show
     @survey = Survey
       .preload(questions: :answers) # This is a bit slower, but sets up the association
-      .find_by_slug!(params[:survey_id])
+      .find_by_slug!(params[:id])
   end
 end
