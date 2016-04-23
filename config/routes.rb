8085f9c38f3668ca5ee6bbc4b081f71a341bbb56
Philip Corliss <pcorliss@50projects.com>
2016-04-22 22:25:11 -0500

    Adding routes, no controllers, or templates yet.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
+
+  resources :surveys, only: [:show] do
+    # should really be part of an answer set
+    # but that model doesn't yet exist
+    resources :answers, only: [:new, :create]
+  end
 end
