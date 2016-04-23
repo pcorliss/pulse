c9db2852ce495a278c37c132a9a3f1cf58b35e10
Philip Corliss <pcorliss@50projects.com>
2016-04-22 23:07:10 -0500

    mini optimization

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,7 +1,7 @@
 class SurveysController < ApplicationController
   def show
     @survey = Survey
-      .preload(:questions, :answers)
+      .preload(questions: :answers) # This is a bit slower, but sets up the association
       .find_by_slug!(params[:survey_id])
   end
 end
